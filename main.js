6970eb7af8b8336701b27aa757c6efc1e51e13e8
Okedinero <ainadipo13@gmail.com>
2025-05-09 00:54:18 -1100

    second

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
-// const rateStatus = document.querySelector(".rating-status");
-// const thankYou = document.querySelector(".thank-you");
-// const form = document.querySelector(".rating-form");
-// const errorMsg = document.getElementById("#error-msg"); // ← new
-
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   const selector = "input[name=ratings]:checked";
-//   const checkedInput = document.querySelector(selector);
-
-//   if (checkedInput !== null) {
-//     const selectedRating = document.querySelector(".selection");
-//     selectedRating.textContent =
-//       "You selected " + checkedInput.getAttribute("value") + " out of 5";
-//     errorMsg.style.display = "none"; // ← hide error
-//     rateStatus.classList.add("hidden");
-//     thankYou.classList.remove("hidden");
-//   } else {
-//     errorMsg.style.display = "block"; // ← show error
-//   }
-// });
-
 const rateStatus = document.querySelector(".rating-status");
 const thankYou = document.querySelector(".thank-you");
 const form = document.querySelector(".rating-form");
+const errorMsg = document.getElementById("#error-msg"); // ← new
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -33,8 +12,10 @@ form.addEventListener("submit", function (e) {
     const selectedRating = document.querySelector(".selection");
     selectedRating.textContent =
       "You selected " + checkedInput.getAttribute("value") + " out of 5";
+    errorMsg.style.display = "none"; // ← hide error
     rateStatus.classList.add("hidden");
     thankYou.classList.remove("hidden");
   } else {
+    errorMsg.style.display = "block"; // ← show error
   }
 });
